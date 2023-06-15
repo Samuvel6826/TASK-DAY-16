1d9534a9f69cc77576974b8bf2146464c0090c70
Samuvel6826 <samuvel6826@gmail.com>
2023-06-15 16:16:09 +0530

    CALL BACK HELL

---
+++ script.js
@@ -0,0 +1,60 @@
+let h1 = document.createElement("h1")
+h1.style.textAlign = "center"
+h1.style.fontSize = "125px"
+document.body.appendChild(h1)
+
+setTimeout(() => {
+
+    h1.innerHTML = "10"
+
+        setTimeout(() => {
+        h1.innerHTML = "9"
+
+            setTimeout(() => {
+            h1.innerHTML = "8"
+
+                setTimeout(() => {
+                h1.innerHTML = "7"
+
+                    setTimeout(() => {
+                    h1.innerHTML = "6"
+
+                        setTimeout(() => {
+                        h1.innerHTML = "5"
+
+                            setTimeout(() => {
+                            h1.innerHTML = "4"
+
+                                setTimeout(() => {
+                                h1.innerHTML = "3"
+
+                                    setTimeout(() => {
+                                    h1.innerHTML = "2"
+
+                                        setTimeout(() => {
+                                        h1.innerHTML = "1"
+
+                                            setTimeout(() => {
+                                            h1.innerHTML = "Happy Independence Day"
+                                                    
+                                            }, 1000);
+                                                
+                                        }, 1000);
+
+                                    }, 1000);
+
+                                }, 1000);
+
+                            }, 1000);
+                            
+                        }, 1000);
+
+                    }, 1000);
+
+                }, 1000);
+
+            }, 1000);
+
+        }, 1000);
+
+    }, 1000);
